32d131d8cbe4b524fb9cfae2181f5fe798b8b4ab
Gaboza18 <91782552+Gaboza18@users.noreply.github.com>
2022-01-17 11:30:38 +0900

    22.01.17.11:30

--- 21_ShopMall/src/main/java/com/green/view/MemberController.java
+++ 21_ShopMall/src/main/java/com/green/view/MemberController.java
@@ -1,19 +1,53 @@
 package com.green.view;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.green.biz.dto.MemberVO;
+import com.green.biz.member.MemberService;
+
 @Controller
 public class MemberController {
+
+	@Autowired
+	private MemberService memberService;
 
 	@GetMapping(value = "/contract")
 	public String contracView() {
 		return "member/contract";
 	}
 
-	@PostMapping(value = "join_form")
+	@PostMapping(value = "/join_form")
 	public String joinView() {
 		return "member/join";
+	}
+
+	/*
+	 * ID 중복 체크 화면 출력
+	 */
+
+	@GetMapping(value = "/id_check_form") // action= id_check_form
+	public String idCheckView(MemberVO vo, Model model) {
+
+		model.addAttribute("id", vo.getId());
+
+		return "member/idcheck";
+	}
+
+	/*
+	 * ID 중복 체크 수행
+	 */
+
+	@PostMapping(value = "/id_check_form")
+	public String idCheckAction(MemberVO vo, Model model) {
+		
+		int result = memberService.confirmID(vo.getId());
+
+		model.addAttribute("message", result);
+
+		return "member/idcheck";
 	}
 }
--- 21_ShopMall/src/main/webapp/WEB-INF/views/member/member.js
+++ 21_ShopMall/src/main/webapp/WEB-INF/views/member/member.js
@@ -8,10 +8,28 @@
 
 function go_next() {
 
-	if ($(".agree")[0].checked == true) { // 클래스 agree가 배열 0번쨰 항목이 동의함 체크되어 있을떄
+	if ($(".agree")[0].checked == true) { // 클래스 agree가 배열 0번쨰 항목이 동의함 체크되어
+											// 있을떄
 		$("#join").attr('action', 'join_form').submit(); // 회원가입 페이지로 이동
 	} else if ($(".agree")[1].checked == true) { // 배열 1번쨰 항목이 체크 되어 있을때
 		alert("약관에 동의해 주세요");
 	}
 
+}
+
+/*
+ * id 중복확인 화면 출력
+ */
+
+function idcheck() {
+
+	if ($("#id").val() == "") { // id 입력하지 않았을떄
+		alert("아이디를 입력하세요");
+		$("#id").focus();
+		return false;
+	}
+
+	// id가 입력이 되었으면 id 중복확인 윈도우 창 오픈(윈도우창 크기 및 사이즈 변경 여부)
+	var url = "id_check_form?id=" + $("#id").val();
+	window.open(url, "_blank_", "toolbar=no, menubar=no, scrollbars=no, resizable=yes, width=350, height=200");
 }
