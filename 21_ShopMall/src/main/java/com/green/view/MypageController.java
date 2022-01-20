ce45efe20fada244e72dc7f71abf5e24314e5fa4
Gaboza18 <91782552+Gaboza18@users.noreply.github.com>
2022-01-20 10:47:44 +0900

    22.01.20.10:47

--- 21_ShopMall/src/main/java/com/green/view/MypageController.java
+++ 21_ShopMall/src/main/java/com/green/view/MypageController.java
@@ -1,5 +1,6 @@
 package com.green.view;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -148,5 +149,70 @@ public class MypageController {
 			// (5) 화면 호출
 			return "mypage/orderList";
 		}
+	}
+
+	@GetMapping(value = "/mypage")
+	public String myPageView(HttpSession session, Model model) {
+
+		// 세션에 저장된 로그인 정보 읽어옴
+		MemberVO loginUser = (MemberVO) session.getAttribute("loginUser");
+
+		if (loginUser == null) {
+			return "member/login";
+		} else {
+
+			// 사용자가 주문한 모든 주문번호 조회
+			OrderVO vo = new OrderVO();
+
+			vo.setId(loginUser.getId());
+			vo.setResult("1");
+
+			List<Integer> oseqList = orderService.selectSeqOrdering(vo);
+
+			// 각 주문번호를 조회하여 주문요약정보 생성
+
+			// (1) 주문 요약 정보 저장 변수
+			List<OrderVO> orderList = new ArrayList<OrderVO>(); // 주문번호 목록 저장
+
+			// (2) 모든 주문번호에 대한 요약정보 생성
+			for (int oseq : oseqList) {
+
+				OrderVO orderVO = new OrderVO();
+
+				orderVO.setId(loginUser.getId());
+				orderVO.setOseq(oseq);
+				orderVO.setResult("1");
+
+				// 각 주문에 대한 주문내역 조회(사용자 ID에 대한 주문내역)
+				List<OrderVO> listByOseq = orderService.listOrderById(orderVO);
+
+				// 위에서 주문한 주문내역의 요약정보 생성
+				OrderVO order = new OrderVO();
+
+				order.setOseq(listByOseq.get(0).getOdseq());
+				order.setIndate(listByOseq.get(0).getIndate());
+
+				if (listByOseq.size() > 1) { // 주문내역 2건 이상 일경우
+					order.setPname(listByOseq.get(0).getPname() + " 외" + (listByOseq.size() - 1) + "건");
+				} else {
+					order.setPname(listByOseq.get(0).getPname()); // 주문내역 1건 이하 일경우
+				}
+
+				// 주문번호별 총액 계산
+				int amount = 0;
+				for (int i = 0; i < listByOseq.size(); i++) { // n번 주문에 대해 계산(n번 주문 외 n건에 대해 계산)
+					amount += listByOseq.get(i).getQuantity() * listByOseq.get(i).getPrice2();
+				}
+
+				order.setPrice2(amount);
+
+				// 요약정보를 List 변수에 추가
+				orderList.add(order); // n번 주문외 n건 n원 값 리스트로 담는다
+			}
+			model.addAttribute("title", "진행중인 주문내역");
+			model.addAttribute("orderList", orderList);
+
+		}
+		return "mypage/mypage";
 	}
 }
