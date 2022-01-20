cc3a9e1b24e77b4ba3d19c0c754625b2a811bb2d
Gaboza18 <91782552+Gaboza18@users.noreply.github.com>
2022-01-20 16:51:47 +0900

    22.01.20.16:51

--- 21_ShopMall/src/main/java/com/green/view/QnaController.java
+++ 21_ShopMall/src/main/java/com/green/view/QnaController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.green.biz.dto.MemberVO;
 import com.green.biz.dto.QnaVO;
@@ -39,13 +40,13 @@ public class QnaController {
 			return "qna/qnaList";
 		}
 	}
-	
+
 	/*
-	 * 게시판 글쓰기  
+	 * 게시판 글쓰기 불러오기
 	 */
-	
+
 	@GetMapping(value = "/qna_write_form")
-	public String qnaWrite(HttpSession session, Model model) {
+	public String qnaWriteView(HttpSession session) {
 
 		MemberVO loginUser = (MemberVO) session.getAttribute("loginUser");
 
@@ -56,4 +57,45 @@ public class QnaController {
 		}
 	}
 
+	/*
+	 * 게시판 글등록
+	 */
+
+	@PostMapping(value = "/qna_write")
+	public String qnaWrite(HttpSession session, QnaVO vo) {
+
+		MemberVO loginUser = (MemberVO) session.getAttribute("loginUser");
+
+		if (loginUser == null) {
+			return "member/login";
+		} else {
+
+			vo.setId(loginUser.getId()); // 화면에서 읽어온 파라미터(subject, content) 사용자 아이디 정보 QnaVO 객체 저장
+			qnaService.insertQna(vo); // qnaService 객체에서 insertQna(qnaVO id) 호출 게시글 저장
+
+			return "redirect:qna_list";
+		}
+	}
+
+	/*
+	 * 게시글 상세보기
+	 */
+	
+	@GetMapping(value = "/qna_view")
+	public String qnaView(HttpSession session, QnaVO vo, Model model) {
+
+		MemberVO loginUser = (MemberVO) session.getAttribute("loginUser");
+
+		if (loginUser == null) {
+			return "member/login";
+		} else {
+
+			vo.setId(loginUser.getId());
+
+			QnaVO qna = qnaService.getQna(vo.getQseq()); // qnaService 객체에서 getQna() 실행하여 조회한 결과 qnaVO 키로 model 객체에 저장
+			model.addAttribute("qnaVO", qna);
+
+			return "qna/qnaView";
+		}
+	}
 }
